628d29e5d6e6c46aea27c617175f62137e33695b
Brian Cao <brian.cao@karma3.com.au>
2020-06-03 09:21:35 +1000

    Hab2, 3, 4 upload function working

---
+++ code_test.py
@@ -0,0 +1,4 @@
+
+topic = 'Habs/Hab2/ExtractionFan/FanSpeedAtTemperature'
+
+print(int(topic.split('/')[1][-1]))
--- pi_things_board_demo.py
+++ pi_things_board_demo.py
@@ -4,6 +4,11 @@ from Habs import Habs
 
 hab1 = Habs('Hab1')
 hab2 = Habs('Hab2')
+hab3 = Habs('Hab3')
+hab4 = Habs('Hab4')
+
+
+hab_list = [hab1, hab2, hab3, hab4]
 
 THINGSBOARD_HOST = 'ec2-13-55-186-158.ap-southeast-2.compute.amazonaws.com'
 ACCESS_TOKEN = 'CSndajbEyIhDajkjc8OM'
@@ -15,6 +20,13 @@ LOCAL_PW = '2016Karma3'
 
 
 hab2_state = {"Hab2":{"Attribute1":True }}
+hab3_state = {"Hab2":{"Attribute1":True }}
+# This function is used to strip the hab number from hab name string. e.g. 'hab1' will return 0, so we can call hab_list[0]. 
+def _get_hab_index(hab_name):
+    return int(hab_name[-1]) - 1
+
+
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, rc, *extra_params):
     print('Connected with result code ' + str(rc))
@@ -25,9 +37,7 @@ def on_connect(client, userdata, rc, *extra_params):
     client.publish('v1/gateway/attributes', json.dumps(hab2_state), 1)
     
     client.publish('v1/gateway/connect', json.dumps({"device":"Hab2"}), 1)
-
-
-
+    client.publish('v1/gateway/connect', json.dumps({"device":"Hab3"}), 1)
     
 
 # The callback for when a PUBLISH message is received from the server.
@@ -45,34 +55,131 @@ def on_message(client, userdata, msg):
     #     set_gpio_status(data['params']['pin'], data['params']['enabled'])
     #     client.publish(msg.topic.replace('request', 'response'), get_gpio_status(), 1)
     #     client.publish('v1/devices/me/attributes', get_gpio_status(), 1)
-    
+    hab_index = _get_hab_index(device)
 
-    if device == 'Hab2':
-        if data['method'] == 'setFanSpeedAtTemp' : 
-            # Parse the message. 
-            fanSpeedAtTemp = float(data['params'])
-            client_local.publish('Habs/Hab2/ExtractionFan/FanSpeedAtTemperature', fanSpeedAtTemp, retain= True)
-        elif data['method'] == "getFanSpeedAtTemp":
-            reply_msg = {'device': device, 'id': data['id'], 'data':  float(hab2.fanSpeedAtTemp)}
-            temp = json.dumps(reply_msg) 
-            print("reply message: " + temp)
-            client.publish('v1/gateway/rpc', json.dumps(reply_msg))
-            
+    # For the at temp control knob on the TB_dashboard for all 4 habs. 
+    if data['method'] == 'setFanSpeedAtTemp' : 
+        # Parse the message. 
+        fanSpeedAtTemp = float(data['params'])
+        hab_list[hab_index]._set_fan_speed_at_temp(data,device)
+        client_local.publish(hab_list[hab_index]._set_fan_speed_at_temp(data,device), fanSpeedAtTemp, retain= True)
 
+    elif data['method'] == "getFanSpeedAtTemp":
+        # reply_msg = {'device': device, 'id': data['id'], 'data':  float(hab2.fanSpeedAtTemp)}
+        # temp = json.dumps(reply_msg) 
+        tb_topic = hab_list[ _get_hab_index(device)]._upload_speed_at_temp(data, device)
+        print(tb_topic)
+        client.publish('v1/gateway/rpc', tb_topic)
     
+    #For the temp low control knob on the 
+    elif data['method'] == 'setFanSpeedLowTemp' : 
+        # Parse the message. 
+        fanSpeedLowTemp = float(data['params'])
+        temp = hab_list[hab_index]._set_fan_speed_low_temp(data,device)
+        client_local.publish(temp, fanSpeedLowTemp, retain= True)
+    elif data['method'] == "getFanSpeedLowTemp":
+        # reply_msg = {'device': device, 'id': data['id'], 'data':  float(hab2.fanSpeedAtTemp)}
+        # temp = json.dumps(reply_msg) 
+        tb_topic = hab_list[ _get_hab_index(device)]._upload_speed_low_temp(data, device)
+        print(tb_topic)
+        client.publish('v1/gateway/rpc', tb_topic)  
+    
+    # For fan speed temp high knob. 
+    elif data['method'] == 'setFanSpeedHighTemp' : 
+        # Parse the message. 
+        fanSpeedHighTemp = float(data['params'])
+        local_topic = hab_list[hab_index]._set_fan_speed_high_temp(data,device)
+        client_local.publish(local_topic, fanSpeedHighTemp, retain= True)
+    elif data['method'] == "getFanSpeedHighTemp":
+        # reply_msg = {'device': device, 'id': data['id'], 'data':  float(hab2.fanSpeedAtTemp)}
+        # temp = json.dumps(reply_msg) 
+        tb_topic = hab_list[ _get_hab_index(device)]._upload_speed_high_temp(data, device)
+        print(tb_topic)
+        client.publish('v1/gateway/rpc', tb_topic)  
+
+     # For extraction fan target temperature setting knob
+    elif data['method'] == 'setSetTemp' : 
+        # Parse the message. 
+        fanSpeedHighTemp = float(data['params'])
+        for x in hab_list[hab_index]._set_set_temp(data,device):
+            client_local.publish(x, fanSpeedHighTemp, retain= True)
+    elif data['method'] == "getSetTemp":
+        # reply_msg = {'device': device, 'id': data['id'], 'data':  float(hab2.fanSpeedAtTemp)}
+        # temp = json.dumps(reply_msg) 
+        tb_topic = hab_list[ _get_hab_index(device)]._upload_set_temp(data, device)
+        print(tb_topic)
+        client.publish('v1/gateway/rpc', tb_topic)  
+
+    elif data['method'] == 'setAutoMode' : 
+        # Parse the message.
+        autoMode = 0  
+        if data['params']: autoMode = 1 
+        x = hab_list[hab_index]._set_auto_mode(data,device)
+
+
+        print(x)
+        print(autoMode)
+        client_local.publish(x, autoMode, retain= True)
+    elif data['method'] == "getAutoMode":
+        # reply_msg = {'device': device, 'id': data['id'], 'data':  float(hab2.fanSpeedAtTemp)}
+        # temp = json.dumps(reply_msg) 
+        tb_topic = hab_list[ _get_hab_index(device)]._upload_auto_mode(data, device)
+        print(tb_topic)
+        client.publish('v1/gateway/rpc', tb_topic)  
+
+    elif data['method'] == 'setManualFanSpeed' : 
+        # Parse the message.
+        manualFanSpeed = float(data['params'])
+        x = hab_list[hab_index]._set_manual_fan_speed(data,device)
+
+        # Check if automode is 0. Only change the fan speed if the hab fan is in manual mode. 
+        if hab_list[hab_index].automode ==0:
+            client_local.publish(x, manualFanSpeed, retain= True)
+    elif data['method'] == "getManualFanSpeed":
+        # reply_msg = {'device': device, 'id': data['id'], 'data':  float(hab2.fanSpeedAtTemp)}
+        # temp = json.dumps(reply_msg) 
+        tb_topic = hab_list[ _get_hab_index(device)]._upload_manual_fan_speed(data, device)
+        print(tb_topic)
+        client.publish('v1/gateway/rpc', tb_topic)      
     return
 
             
 
 def on_connect_local(client_local, userdata, rc, *extra_params):
     print('Connected to local Pi MQTT with result code ' + str(rc))
-    client_local.subscribe('Habs/Hab2/ExtractionFan/+')
+    client_local.subscribe('Habs/+/ExtractionFan/+')
     return
 
 def on_message_local(client_local, userdata, msg):
 
-    if msg.topic == 'Habs/Hab2/ExtractionFan/FanSpeedAtTemperature':
-        hab2.fanSpeedAtTemp = msg.payload 
+    topic_list = msg.topic.split('/')
+    device = topic_list[1]
+    hab_index = _get_hab_index(device)
+
+
+    # if msg.topic == 'Habs/Hab2/ExtractionFan/FanSpeedAtTemperature':
+    #     hab2.fanSpeedAtTemp = msg.payload 
+
+    # Check if the 3rd level of the topic is ExtractionFan. 
+    if topic_list[2] == 'ExtractionFan':
+        if topic_list[3] == 'FanSpeedAtTemperature':
+            hab_list[hab_index].fanSpeedAtTemp = msg.payload
+        elif topic_list[3] == 'FanSpeedTemperatureLow':
+            hab_list[hab_index].fanSpeedTempLow = msg.payload
+        elif topic_list[3] == 'FanSpeedTemperatureHigh':
+            hab_list[hab_index].fanSpeedTempHigh = msg.payload
+        elif topic_list[3] == 'FanTemperatureSetPoint':
+            hab_list[hab_index].tempSetPoint = msg.payload
+        elif topic_list[3] == 'TemperatureSetPoint':
+            hab_list[hab_index].tempSetPoint = msg.payload
+        elif topic_list[3] == 'AutomaticMode':
+            hab_list[hab_index].automode = int(msg.payload)
+        elif topic_list[3] == 'Speed':
+            hab_list[hab_index].manualFanSpeed = int(msg.payload)
+            
+           
+
+
 
     return
 
